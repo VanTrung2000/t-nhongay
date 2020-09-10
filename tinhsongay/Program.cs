9ae9e9b1260badf495a80a2a67a39b115e726441
VanTrung2000 <70925652+VanTrung2000@users.noreply.github.com>
2020-09-10 20:30:04 +0700

    Update Program.cs

--- tinhsongay/Program.cs
+++ tinhsongay/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace tinhsongay
 {
@@ -7,43 +7,49 @@ namespace tinhsongay
         static void Main(string[] args)
         {
             Console.Write("Nhap thang : ");
-            string Sthang = Console.ReadLine();
+            string thang = Console.ReadLine();
             Console.Write("Nhap nam : ");
-            string Snam = Console.ReadLine();
-
-            int th = int.Parse(Sthang);
-            int nm = int.Parse(Snam);
+            string nam = Console.ReadLine();
             int songay = 0;
+            ngaythang(thang,nam);
 
-            if (th >= 1 && th <= 12)
+            
+         static void ngaythang(string thang, string nam)
             {
-                switch (th)
+                int songay = 0;
+                int th = int.Parse(thang);
+                int nm = int.Parse(nam);
+                if (th >= 1 && th <= 12)
                 {
-                    case 1:
-                    case 3:
-                    case 5:
-                    case 7:
-                    case 8:
-                    case 10:
-                    case 12: songay = 31; break;
-                    case 4:
-                    case 6:
-                    case 9:
-                    case 11: songay = 30; break;
+                    switch (th)
+                    {
+                        case 1:
+                        case 3:
+                        case 5:
+                        case 7:
+                        case 8:
+                        case 10:
+                        case 12: songay = 31; break;
+                        case 4:
+                        case 6:
+                        case 9:
+                        case 11: songay = 30; break;
 
-                    case 2:
-                        if (nm % 400 == 0 || (nm % 4 == 0 && nm % 100 != 0))    // nam nhuan
-                            songay = 29;
-                        else
-                            songay = 28;
-                        break;
+                        case 2:
+                            if (nm % 400 == 0 || (nm % 4 == 0 && nm % 100 != 0))    // nam nhuan
+                                songay = 29;
+                            else
+                                songay = 28;
+                            break;
+                    }
+
+                    Console.Write("=> Thang " + th + "/" + nm + " co " + songay + " ngay\n");
                 }
-
-                Console.Write("=> Thang " + th + "/" + nm + " co " + songay + " ngay\n");
+                else
+                    Console.Write("=> Thang khong hop le!\n");
+                Console.ReadLine();
             }
-            else
-                Console.Write("=> Thang khong hop le!\n");
-            Console.ReadLine();
+
         }
     }
 }
